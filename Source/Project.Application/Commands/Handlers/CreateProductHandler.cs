800dde2bb978b57812e345665d85da68c8b4e081
https-richardy <=>
2024-02-07 02:21:09 -0300

    #feat: integrating AutoMapper for improved object mapping. The TinyMapper dependency has been replaced, and the necessary changes have been made to the constructor and the mapping process.

--- Source/Project.Application/Commands/Handlers/CreateProductHandler.cs
+++ Source/Project.Application/Commands/Handlers/CreateProductHandler.cs
@@ -1,6 +1,6 @@
+using AutoMapper;
 using FluentValidation;
 using MediatR;
-using Nelibur.ObjectMapper;
 using Project.Domain.Contracts;
 using Project.Domain.Entities;
 
@@ -10,13 +10,16 @@ public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand,
 {
     private readonly IRepository<Product> _productRepository;
     private readonly IValidator<CreateProductCommand> _validator;
+    private readonly IMapper _mapper;
 
-    public CreateProductCommandHandler(IRepository<Product> productRepository, IValidator<CreateProductCommand> validator)
+    public CreateProductCommandHandler(IRepository<Product> productRepository, IValidator<CreateProductCommand> validator, IMapper mapper)
     {
         _productRepository = productRepository;
         _validator = validator;
+        _mapper = mapper;
     }
 
+    # pragma warning disable CS8604
     public async Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
         var validationResult = await _validator.ValidateAsync(request);
@@ -24,7 +27,7 @@ public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand,
         if (!validationResult.IsValid)
             throw new ValidationException(validationResult.Errors);
 
-        var product = TinyMapper.Map<Product>(request);
+        var product = _mapper.Map<Product>(request);
         await _productRepository.SaveAsync(product);
 
         return new CreateProductResponse
