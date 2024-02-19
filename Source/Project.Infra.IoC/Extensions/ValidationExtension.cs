a9c88c86eac5982684782fabd7033ca11f4ef5bd
https-richardy <=>
2024-02-19 02:13:37 -0300

    #feat: Expanded the ValidationExtension class in the Project.Infra.IoC.Extensions namespace to include validators for account-related commands and queries.

--- Source/Project.Infra.IoC/Extensions/ValidationExtension.cs
+++ Source/Project.Infra.IoC/Extensions/ValidationExtension.cs
@@ -10,7 +10,18 @@ public static class ValidationExtension
 {
     public static void AddValidation(this IServiceCollection services)
     {
+        # region Product validators
+
         services.AddScoped<IValidator<CreateProductCommand>, CreateProductCommandValidator>();
         services.AddScoped<IValidator<GetAllProductsQuery>, GetAllProductsQueryValidator>();
+
+        # endregion
+
+        # region Account validators
+
+        services.AddScoped<IValidator<CreateAccountCommand>, CreateAccountCommandValidator>();
+        services.AddScoped<IValidator<AuthenticationQuery>, AuthenticationQueryValidator>();
+
+        # endregion
     }
 }
